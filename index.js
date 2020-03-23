18ec0ef8b824f2fe5f0646ac243bb4672451ee44
Radek <radek@MacBook-Pro-Radek.local>
2020-03-23 21:20:33 +0100

    srv init

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("start here ok ok");
+console.log("start here ok ok?");
+const { ApolloServer } = require("apollo-server");
+// czemu nie moge uzyc skladni es6 do importowania w node.js
+
+const typeDefs = `
+    type Query {
+        greeting: String
+    }
+`;
+
+const srv = new ApolloServer({ typeDefs });
+
+srv
+  .listen({ port: 4000 })
+  .then(res =>
+    console.log(`RANDOM-STUFF::: port: ${res.port} | url: ${res.url}`)
+  );
