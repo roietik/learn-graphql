46d2980dd85c4663a7e6b51c8e3de0a6449b64bd
Radek <radek@MacBook-Pro-Radek.local>
2020-03-23 21:41:47 +0100

    Example of data

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-console.log("start here ok ok?");
-const { ApolloServer } = require("apollo-server");
-// czemu nie moge uzyc skladni es6 do importowania w node.js
+const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = `
-    type Query {
-        greeting: String
-    }
+const typeDefs = gql`
+  type Todo {
+    title: String
+    done: Boolean
+    id: Int!
+  }
+
+  type Query {
+    todo: [Todo]
+  }
 `;
 
-const srv = new ApolloServer({ typeDefs });
+const todo = [
+  {
+    title: "Water the flowers",
+    done: true,
+    id: 0
+  },
+  {
+    title: "Empty the dishwasher",
+    done: true,
+    id: 1
+  },
+  {
+    title: "Wash the car",
+    done: true,
+    id: 2
+  },
+  {
+    title: "Learn the GraphQL",
+    done: false,
+    id: 3
+  },
+  {
+    title: "Practice the GSAP",
+    done: false,
+    id: 4
+  }
+];
+
+const resolvers = {
+  Query: {
+    todo: () => todo
+  }
+};
+
+const srv = new ApolloServer({ typeDefs, resolvers });
 
 srv
   .listen({ port: 4000 })
-  .then(res =>
-    console.log(`RANDOM-STUFF::: port: ${res.port} | url: ${res.url}`)
-  );
+  .then(res => console.log(`[🚀 RANDOM-STUFF 🚀] Server ready at ${res.url}`));
